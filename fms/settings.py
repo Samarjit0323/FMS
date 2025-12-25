8033c524cf67ea352300fa3f47353cf86416cd83
Samarjit0323 <samarjit19official@gmail.com>
2025-12-25 16:00:25 +0530

    Updated ALLOWED HOSTS

--- fms/settings.py
+++ fms/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["https://fms-sxhc.onrender.com"]
+ALLOWED_HOSTS = ["fms-sxhc.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
